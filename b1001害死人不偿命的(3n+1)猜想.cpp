d291ecd7312cf218abe9e7ddd23618ba799520fc
dexter96 <39894049+dexter96G@users.noreply.github.com>
2022-04-10 22:56:53 +0800

    PAT乙级B1001害死人不偿命的(3n+1)猜想

---
+++ b1001害死人不偿命的(3n+1)猜想.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n, step = 0;
+	cin >>n;
+	while(n != 1){
+		if(n % 2 == 0){
+			n /= 2;
+			step++;
+		}else {
+			n = (3 * n  + 1) / 2;
+			step ++;
+		}
+	}
+	cout << step;
+	return 0;
+} 
